383ed74f51fc4a62bdb680f9319e8fc8663570ca
monandkey <satoru070505@gmail.com>
2022-10-02 09:23:39 +0900

    [add] character entities

---
+++ lib/entities/character.dart
@@ -0,0 +1,21 @@
+class Character {
+  final String name;
+  final CharacterPattern characterPattern;
+  final bool specialEncounter;
+
+  Character(
+    this.name,
+    this.characterPattern,
+    this.specialEncounter
+  );
+}
+
+class CharacterPattern {
+  final String alias;
+  final String image;
+
+  CharacterPattern(
+    this.alias,
+    this.image
+  );
+}
