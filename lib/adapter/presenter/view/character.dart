fe22fd7673ec25552772f28bb54e4b11f633691f
monandkey <satoru070505@gmail.com>
2022-10-02 21:38:33 +0900

    [add] character view for presenter

---
+++ lib/adapter/presenter/view/character.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// temp
+class CharacterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Please',
+        style: TextStyle(fontSize: 24, color: Colors.black),
+      ),
+    );
+  }
+}
